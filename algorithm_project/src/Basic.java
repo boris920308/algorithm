7cf1f77d378f560c404f44efe809f69bcf850e09
boris920308 <jhoonj0308@gmail.com>
2025-08-20 23:10:08 +0900

    feat. basic. 내림차순 정렬

--- algorithm_project/src/Basic.java
+++ algorithm_project/src/Basic.java
@@ -1,13 +1,21 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Basic {
     public static void main(String[] args) {
         ascendingSort();
+        descendingSort1();
     }
 
     private static void ascendingSort() {
         int[] A = {4, 3, 5, 2, 1};
         Arrays.sort(A); // 오름차순 정렬
+        System.out.println(Arrays.toString(A));
+    }
+
+    private static void descendingSort1() {
+        Integer[] A = {5, 4, 3, 2, 1};
+        Arrays.sort(A, Collections.reverseOrder()); // 내림차순 정렬
         System.out.println(Arrays.toString(A));
     }
 }
